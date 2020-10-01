5fbdf0b1d6a0893784e9fdc6f0af2ac94bc7ac70
vivian0113 <70936691+vivian0113@users.noreply.github.com>
2020-10-01 20:26:25 +1000

    Add SplashActivity.java

---
+++ app/src/main/java/com/example/mobiledemo/SplashActivity.java
@@ -0,0 +1,36 @@
+package com.example.mobiledemo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.example.mobiledemo.ui.login.LoginActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private final int SPLASH_DISPLAY_LENGHT = 2200; // 两秒后进入系统
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new android.os.Handler().postDelayed(new Runnable() {
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this,
+                        LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+            }
+
+        }, SPLASH_DISPLAY_LENGHT);
+
+    }
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//       super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_splash);
+
+//    }
+}
