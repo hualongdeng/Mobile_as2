52656fa3d29d910b1819247f486cdf26306c5d2e
vivian0113 <70936691+vivian0113@users.noreply.github.com>
2020-10-05 01:07:51 +1100

    Update MainActivity.java
    Set icon size

--- app/src/main/java/com/example/mobiledemo/MainActivity.java
+++ app/src/main/java/com/example/mobiledemo/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         BottomNavigationView navView = findViewById(R.id.nav_view);
         navView.setItemIconTintList(null);
+        navView.setItemIconSize(200);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
