8ab31c5281c6adbd2562dd70f68628d7fc784479
chensun01 <57696598+chensun01@users.noreply.github.com>
2020-11-02 18:56:14 +1100

    dialog

--- app/src/main/java/com/example/mobiledemo/ui/account/AccountActivity.java
+++ app/src/main/java/com/example/mobiledemo/ui/account/AccountActivity.java
@@ -16,11 +16,19 @@ import com.example.mobiledemo.R;
 import com.example.mobiledemo.ui.login.LoginActivity;
 import com.example.mobiledemo.ui.password.PasswordActivity;
 
+import android.app.Dialog;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import java.util.Calendar;
-
+import android.util.Log;
+import android.widget.Toast;
 public class AccountActivity extends AppCompatActivity {
 
     private AccountViewModel accountViewModel;
+    private Button uploadButton;
 
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,7 +37,7 @@ public class AccountActivity extends AppCompatActivity {
         final Button backButton = findViewById(R.id.account_back);
         final Button passwordButton = findViewById(R.id.account_password);
         final Button logoutButton = findViewById(R.id.account_logout);
-        final Button uploadButton = findViewById(R.id.account_upload);
+        uploadButton = findViewById(R.id.account_upload);
         final EditText birthdayText = findViewById(R.id.birthday);
 
         backButton.setOnClickListener(new View.OnClickListener() {
@@ -70,8 +78,7 @@ public class AccountActivity extends AppCompatActivity {
         uploadButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(AccountActivity.this, ProfilePhotoActivity.class);
-                startActivity(intent);
+                setDialog();
             }
         });
 
@@ -85,4 +92,41 @@ public class AccountActivity extends AppCompatActivity {
 
     }
 
+    private void setDialog() {
+        Dialog mCameraDialog = new Dialog(this, R.style.BottomDialog);
+        LinearLayout root = (LinearLayout) LayoutInflater.from(this).inflate(
+                R.layout.bottom_dialog, null);
+        //初始化视图
+        root.findViewById(R.id.btn_choose_img).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "click choose image",Toast.LENGTH_SHORT).show();
+            }
+        });
+        root.findViewById(R.id.btn_open_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "click open camera",Toast.LENGTH_SHORT).show();
+            }
+        });
+        root.findViewById(R.id.btn_cancel).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getApplicationContext(), "click cancel",Toast.LENGTH_SHORT).show();
+            }
+        });
+        mCameraDialog.setContentView(root);
+        Window dialogWindow = mCameraDialog.getWindow();
+        dialogWindow.setGravity(Gravity.BOTTOM);
+        WindowManager.LayoutParams lp = dialogWindow.getAttributes(); // 获取对话框当前的参数值
+        lp.x = 0; // 新位置X坐标
+        lp.y = 0; // 新位置Y坐标
+        lp.width = (int) getResources().getDisplayMetrics().widthPixels; // 宽度
+        root.measure(0, 0);
+        lp.height = root.getMeasuredHeight();
+
+        lp.alpha = 9f; // 透明度
+        dialogWindow.setAttributes(lp);
+        mCameraDialog.show();
+    }
 }
